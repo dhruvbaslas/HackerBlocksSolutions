60d7b1804a8ec6a514dc5f8d16772b14181b80bc
Dhruv Baslas <dhruvbaslas@gmail.com>
2020-01-23 00:11:36 +0530

    Update ClassRoom_2247.py

--- ClassRoom_2247.py
+++ ClassRoom_2247.py
@@ -1,14 +1,15 @@
-#ClassRoom_2247
+# ClassRoom_2247
 
 T = [[1, 1], [1, 0]]
 mat = [[1, 0], [1, 0]]
+mod = 1000000006
 
 
 def multiply(T, F):
-    a = T[0][0] * F[0][0] + T[0][1] * F[1][0]
-    b = T[0][0] * F[0][1] + T[0][1] * F[1][1]
-    c = T[1][0] * F[0][0] + T[1][1] * F[1][0]
-    d = T[1][0] * F[0][1] + T[1][1] * F[1][1]
+    a = (((T[0][0] % mod) * (F[0][0] % mod)) % mod + ((T[0][1] % mod) * (F[1][0] % mod)) % mod) % mod
+    b = (((T[0][0] % mod) * (F[0][1] % mod)) % mod + ((T[0][1] % mod) * (F[1][1] % mod)) % mod) % mod
+    c = (((T[1][0] % mod) * (F[0][0] % mod)) % mod + ((T[1][1] % mod) * (F[1][0] % mod)) % mod) % mod
+    d = (((T[1][0] % mod) * (F[0][1] % mod)) % mod + ((T[1][1] % mod) * (F[1][1] % mod)) % mod) % mod
 
     return [[a, b], [c, d]]
 
@@ -31,16 +32,25 @@ def fibonacci(n):
     else:
         z = power(T, n)
         res = multiply(z, mat)
-        return res[0][0], res[1][0]
+        return res[0][0] % mod, res[1][0] % mod
+
+
+def modIt(v, p):
+    if p == 0:
+        return 1
+    if p % 2 != 0:
+        return ((v % (mod + 1)) * (modIt((v * v) % (mod + 1), p // 2))) % (mod + 1)
+    else:
+        return modIt(v * v % (mod + 1), p // 2)
+
 
 t = int(input())
 for _ in range(t):
     arr = list(map(int, input().split()))
-    x = arr[0] + 1
-    y = arr[1] + 1
-    n = arr[2]
-    # print(x, y, n)
-    p1, p2 = fibonacci(n-2)
-    print(p1, p2)
-    result = (x ** p2) * (y ** p1)
-    print(result - 1)
+    x = (arr[0] + 1) % (mod + 1)
+    y = (arr[1] + 1) % (mod + 1)
+    n = (arr[2]) % (mod + 1)
+    p1, p2 = fibonacci(n - 2)
+    result = ((modIt(x, p2) % (mod + 1)) * (modIt(y, p1) % (mod + 1))) % (mod + 1)
+    alpha = (result - 1) % (mod + 1)
+    print(alpha)
